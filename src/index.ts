8dc547a8743f4c0898f78b478a32ecf30a6e9e3d
t <15724094+tdev314@users.noreply.github.com>
2026-04-05 10:50:07 +0000

    Add CORS support for auth API and remove trustedOrigins configuration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { cors } from "hono/cors";
 import { auth } from './lib/auth'
 import { logger } from 'hono/logger'
 const app = new Hono()
@@ -20,6 +21,19 @@ app.get('/health', (c) => {
  * Better Auth routes, see docs before changing
  * @link https://better-auth.com/docs
  */
+
+app.use(
+	"/api/auth/*", // or replace with "*" to enable cors for all routes
+	cors({
+		origin: "http://localhost:3001", // replace with your origin
+		allowHeaders: ["Content-Type", "Authorization"],
+		allowMethods: ["POST", "GET", "OPTIONS"],
+		exposeHeaders: ["Content-Length"],
+		maxAge: 600,
+		credentials: true,
+	}),
+);
+
 app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
 
 export default app
